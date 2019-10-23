ec70b111893c70eafe5fb91073d229ae9c4d18bd
Kenneth-ca <kenneth.ca95@gmail.com>
2019-10-23 04:17:51 +0000

    [config] for vagrant

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,4 +59,8 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  #Config for vagrant (10.0.2.2)
+  class Application < Rails::Application
+    config.web_console.whitelisted_ips = '10.0.2.2'
+  end
 end
